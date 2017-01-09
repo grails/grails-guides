cd99ea9a9341ee2621db5e1732e4dc23ec5ea558
Zachary Klein <zak@silver-chalice.com>
2017-01-09 09:59:14 -0600

    Basic support for specifying a profile

--- create-guide.sh
+++ create-guide.sh
@@ -1,20 +1,22 @@
 
 GUIDE_NAME="$1"
+PROFILE="$2"
+PROFILE_NAME="$3"
 
 if [[ -n $GUIDE_NAME ]]; then
     mkdir $GUIDE_NAME
     cd $GUIDE_NAME
-
-    grails create-app $GUIDE_NAME
+    echo
+    grails create-app $GUIDE_NAME $PROFILE $PROFILE_NAME
     mv $GUIDE_NAME initial
     mkdir complete
     cp -rf initial/* complete/
     echo "include 'complete'" > settings.gradle
     cp -rf ../src/main/project/* .
     cp ../src/main/project/.travis.yml .travis.yml
-    gradle wrapper   
+    gradle wrapper
 
-    echo "Guide created at location $GUIDE_NAME/" 
+    echo "Guide created at location $GUIDE_NAME/"
 else
     echo "Please specify the guide name (all lower case, hyphen separated). Example: ./create-guide foo-bar"
-fi    
+fi
