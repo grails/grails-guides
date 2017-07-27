dd0110e852fa6211a8d8fb60eefb2c937d2e1b1e
sdelamo <sergio.delamo@softamo.com>
2017-07-27 17:00:49 +0200

    Revert "working suggestion form using HTML Imports"
    This reverts commit 344096eedd133ecca2d4937fb05a9d6289bb834f.
    
    # Conflicts:
    #	.gitignore

--- src/HTMLImport.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-
-    let getImport = document.querySelector('link[rel="import"]');
-
-    let getContent = getImport.import.querySelector('#mauticform_wrapper_grailsorgguidesuggestionform');
-
-    document.getElementById("suggestion-form").appendChild(getContent.cloneNode(true));
-};
