1f6887c36b8423729360f415251c9f1ca1a2f5fc
sdelamo <sergio.delamo@softamo.com>
2017-08-09 07:04:23 +0000

    Publishing Grails Guide for Travis Build: https://travis-ci.org/grails-guides/grails-restapi-angularjs/builds/262548594

--- grails-restapi-angularjs/js/navbar.js
+++ grails-restapi-angularjs/js/navbar.js
@@ -1,10 +1,9 @@
 function toggleNavIcon() {
 	if ( document.getElementById('nav-icon').className === "open" ) {
 		document.getElementById('nav-icon').className = "";	
-		document.getElementById('navbar-nav').classList.add("closemobile");								
+		document.getElementById('grailsnavbar-nav').classList.add("closemobile");
 	} else {
 		document.getElementById('nav-icon').className += "open";	
-		document.getElementById('navbar-nav').classList.remove("closemobile");					
-	}	 
-console.log("class: " + document.getElementById('navbar-nav').className);
+		document.getElementById('grailsnavbar-nav').classList.remove("closemobile");
+	}
 }
